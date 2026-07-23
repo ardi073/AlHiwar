e36c33f42cbaa164b77f2d88b104e88cb4b879dc
ardi073 <ardi073@gmail.com>
2026-07-23 15:34:36 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Al-Hiwar Service Worker v26.0
-const CACHE_NAME = 'al-hiwar-v26';
+// Al-Hiwar Service Worker v27.0
+const CACHE_NAME = 'al-hiwar-v27';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
